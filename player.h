23b3ad555de5f97c26ac7fa1961fa4fbfd96044f
OL <1613661434@qq.com>
2026-02-08 22:08:07 +0800

    10. 游戏玩家类-part2

--- player.h
+++ player.h
@@ -2,6 +2,7 @@
 #define PLAYER_H
 
 #include <QObject>
+#include "cards.h"
 
 class Player : public QObject
 {
@@ -42,6 +43,9 @@ private:
     Type m_type;
     int m_score;
     bool m_isWin;
+    Player* m_prev;
+    Player* m_next;
+    Cards m_cards;
 
     // == 函数 ==
 public:
@@ -50,32 +54,50 @@ public:
 
     // 名字
     inline void setName(QString name) { m_name = name; }
-    inline QString getName() { return m_name; }
+    inline QString getName() const { return m_name; }
 
     // 角色
     inline void setRole(Role role) { m_role = role; }
-    inline Role getRole() { return m_role; }
+    inline Role getRole() const { return m_role; }
 
     // 性别
     inline void setSex(Sex sex) { m_sex = sex; }
-    inline Sex getSex() { return m_sex; }
+    inline Sex getSex() const { return m_sex; }
 
     // 头像的显示方位
     inline void setDirection(Direction direction) { m_direction = direction; }
-    inline Direction getDirection() { return m_direction; }
+    inline Direction getDirection() const { return m_direction; }
 
     // 玩家的类型
     inline void setType(Type type) { m_type = type; }
-    inline Type getType() { return m_type; }
+    inline Type getType() const { return m_type; }
 
     // 分数
     inline void setScore(int score) { m_score = score; }
-    inline int getScore() { return m_score; }
+    inline int getScore() const { return m_score; }
 
     // 游戏结果
     inline void setIsWin(bool flag) { m_isWin = flag; }
-    inline bool getIsWin() { return m_isWin; }
+    inline bool getIsWin() const { return m_isWin; }
 
+    // 游戏上家
+    inline void setPrevPlayer(Player* prev) { m_prev = prev; }
+    inline Player* getPrevPlayer() const { return m_prev; }
+
+    // 游戏下家
+    inline void setNextPlayer(Player* next) { m_next = next; }
+    inline Player* getNextPlayer() const { return m_next; }
+
+    // 抢地主
+    inline void grabLordBet(int point) { /*WILL DO*/ };
+
+    // 存储扑克牌（发牌阶段）
+    inline void storeDispatchCard(Card& card) { m_cards.add(card); }
+    inline void storeDispatchCard(Cards& cards) { m_cards.add(cards); }
+
+    inline Cards getCards() const { return m_cards; }             // 得到所有牌
+    inline void clearCards() { m_cards.clear(); }                 // 清空所有牌
+    inline void playHand(Cards& cards) { m_cards.remove(cards); } // 出牌
 signals:
 };
 
