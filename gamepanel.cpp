314f410277cdd3868b95cf0ccd50cf9268a307f2
OL <1613661434@qq.com>
2026-03-07 11:08:25 +0800

    22. 游戏主窗口初始化分析以及背景图的绘制

--- gamepanel.cpp
+++ gamepanel.cpp
@@ -1,5 +1,6 @@
 #include "gamepanel.h"
 #include "ui_gamepanel.h"
+#include <QRandomGenerator>
 
 GamePanel::GamePanel(QWidget* parent)
     : QMainWindow(parent), ui(new Ui::GamePanel)
@@ -8,9 +9,28 @@ GamePanel::GamePanel(QWidget* parent)
 
     ui->btnGroup->initButtons();
     ui->btnGroup->selectPanel(ButtonGroup::Panel::Start);
+
+    // 1. 背景图
+    int num = QRandomGenerator::global()->bounded(1, 11); // 1-10
+    QString path = QString(":/images/background-%1.png").arg(num);
+    m_bkImage.load(path);
+    // 2. 窗口的标题的大小
+    this->setWindowTitle("欢乐斗地主");
+    this->setFixedSize(1000, 650);
+    // 3. 实例化游戏控制类对象
+    // 4. 玩家得分
+    // 5. 切割游戏图片
+    // 6. 初始化游戏中的按钮组
+    // 7. 初始化玩家在窗口中的上下文环境
 }
 
 GamePanel::~GamePanel()
 {
     delete ui;
+}
+
+void GamePanel::paintEvent(QPaintEvent* ev)
+{
+    QPainter p(this);
+    p.drawPixmap(rect(), m_bkImage);
 }
--- gamepanel.h
+++ gamepanel.h
@@ -2,22 +2,30 @@
 #define GAMEPANEL_H
 
 #include <QMainWindow>
+#include <QPainter>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class GamePanel;
+namespace Ui
+{
+    class GamePanel;
 }
 QT_END_NAMESPACE
 
 class GamePanel : public QMainWindow
 {
     Q_OBJECT
+private:
+    QPixmap m_bkImage;
 
 public:
-    GamePanel(QWidget *parent = nullptr);
+    GamePanel(QWidget* parent = nullptr);
     ~GamePanel();
 
+protected:
+    // 绘图
+    void paintEvent(QPaintEvent* ev) override;
+
 private:
-    Ui::GamePanel *ui;
+    Ui::GamePanel* ui;
 };
 #endif // GAMEPANEL_H
